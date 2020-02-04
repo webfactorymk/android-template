033eaa15319c7de7b5c10bc7b3d90997f95432b5
WF | Dimitar Zabaznoski <dimitar.zabaznoski@webfactory.mk>
2020-02-04 16:31:32 +0100

    Add AndroidX's PreferenceManager

--- app/src/main/java/mk/webfactory/template/data/DataModule.kt
+++ app/src/main/java/mk/webfactory/template/data/DataModule.kt
@@ -2,7 +2,7 @@ package mk.webfactory.template.data
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.preference.PreferenceManager
+import androidx.preference.PreferenceManager
 import dagger.Module
 import dagger.Provides
 import mk.webfactory.template.di.qualifier.ApplicationContext
@@ -12,7 +12,7 @@ import javax.inject.Singleton
 class DataModule {
     @Provides
     @Singleton
-    fun provideSharedPreferences(@ApplicationContext context: Context?): SharedPreferences {
+    fun provideSharedPreferences(@ApplicationContext context: Context): SharedPreferences {
         return PreferenceManager.getDefaultSharedPreferences(context)
     }
 }
