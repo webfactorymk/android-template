9ce0aeb94f3eb07c17374088ea5ea7237f5be8a6
WF | Dimitar Zabaznoski <dimitar.zabaznoski@webfactory.mk>
2020-02-04 14:50:41 +0100

    Cleanup App class

--- app/src/main/java/mk/webfactory/template/App.kt
+++ app/src/main/java/mk/webfactory/template/App.kt
@@ -1,34 +1,29 @@
 package mk.webfactory.template
 
 import android.app.Activity
-import android.content.Context
-import android.support.multidex.MultiDex
-import com.crashlytics.android.Crashlytics
 import com.jakewharton.threetenabp.AndroidThreeTen
-import com.squareup.leakcanary.LeakCanary
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 import dagger.android.DispatchingAndroidInjector
-import io.fabric.sdk.android.Fabric
-import io.fabric.sdk.android.SilentLogger
-import mk.webfactory.androidtemplate.BuildConfig
 import mk.webfactory.template.di.AppComponent
 import mk.webfactory.template.di.DaggerAppComponent
 import mk.webfactory.template.log.CrashReportLogger
-import mk.webfactory.template.log.CrashlyticsLogger
+import mk.webfactory.template.log.DebugLogger
 import timber.log.Timber
 
 class App : DaggerApplication() {
 
     companion object {
-        lateinit var CRASH_REPORT: CrashReportLogger
+        val CRASH_REPORT: CrashReportLogger by lazy { crashReportLogger }
+        private lateinit var crashReportLogger: CrashReportLogger
     }
 
     private var appComponent: AppComponent? = null
+
     override fun onCreate() {
         super.onCreate()
         AndroidThreeTen.init(this)
-        initializeLoggingEnvironment()
+        crashReportLogger = initializeLoggingEnvironment()
     }
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
@@ -45,26 +40,27 @@ class App : DaggerApplication() {
         }
     }
 
-    private fun initializeLoggingEnvironment() {
+    private fun initializeLoggingEnvironment(): CrashReportLogger {
         if (!BuildConfig.DEBUG) {
 
             //Fixme: use firebase crashlytics
 
-            val crashlytics = Crashlytics()
-            Fabric.with(
-                Builder(this)
-                    .logger(SilentLogger())
-                    .kits(crashlytics)
-                    .build()
-            )
-            val logger = CrashlyticsLogger(this, crashlytics)
-            Timber.plant(logger)
-            CRASH_REPORT = logger
+            throw UnsupportedOperationException("Not imeplemented yet")
+
+            // val crashlytics = Crashlytics()
+            // Fabric.with(
+            //     Builder(this)
+            //         .logger(SilentLogger())
+            //         .kits(crashlytics)
+            //         .build()
+            // )
+            // val logger = CrashlyticsLogger(this, crashlytics)
+            // Timber.plant(logger)
+            // return logger
         } else {
-            val logger: mk.webfactory.template.log.DebugLogger =
-                mk.webfactory.template.log.DebugLogger()
+            val logger = DebugLogger()
             Timber.plant(logger)
-            CRASH_REPORT = logger
+            return logger
         }
     }
 }
