68abd3eee430707022fdc665a395f33fcf13b678
WF | Dimitar Zabaznoski <dimitar.zabaznoski@webfactory.mk>
2020-02-05 00:52:17 +0100

    Translated dagger qualifiers and scopes

---
+++ app/src/main/java/mk/webfactory/template/di/qualifier/ApplicationContext.kt
@@ -0,0 +1,7 @@
+package mk.webfactory.template.di.qualifier
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ApplicationContext
---
+++ app/src/main/java/mk/webfactory/template/di/qualifier/Remote.kt
@@ -0,0 +1,8 @@
+package mk.webfactory.template.di.qualifier
+
+import javax.inject.Qualifier
+
+@Qualifier
+@MustBeDocumented
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Remote
