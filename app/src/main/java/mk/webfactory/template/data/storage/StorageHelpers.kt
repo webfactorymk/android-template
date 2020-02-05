36e43b2b196fa69fbfe1df268a9368b144d0e9f7
PetrovskiM <38075899+PetrovskiM@users.noreply.github.com>
2020-02-05 17:25:54 +0100

    Convert from object to top level functions

--- app/src/main/java/mk/webfactory/template/data/storage/StorageHelpers.kt
+++ app/src/main/java/mk/webfactory/template/data/storage/StorageHelpers.kt
@@ -1,29 +1,27 @@
 package mk.webfactory.template.data.storage
 
-import io.reactivex.Observable
+import io.reactivex.Single
 import io.reactivex.functions.Function
 
-object StorageHelpers {
-    fun <T> deferSaveIn(storage: Storage<T>): Function<T, Observable<T>> {
-        return deferSaveIn(storage, false)
-    }
+fun <T> deferSaveIn(storage: Storage<T>): Function<T, Single<T>> {
+    return deferSaveIn(storage, false)
+}
 
-    /**
-     * Unlike [deferSaveIn], the create observable will not be allowed to throw,
-     * and instead the object being saved will be returned when an error occurs.
-     */
-    fun <T> deferSaveInAndIgnoreErrors(storage: Storage<T>): Function<T, Observable<T>> {
-        return deferSaveIn(storage, true)
-    }
+/**
+ * Unlike [deferSaveIn], the create observable will not be allowed to throw,
+ * and instead the object being saved will be returned when an error occurs.
+ */
+fun <T> deferSaveInAndIgnoreErrors(storage: Storage<T>): Function<T, Single<T>> {
+    return deferSaveIn(storage, true)
+}
 
-    private fun <T> deferSaveIn(
-        storage: Storage<T>,
-        continueOnError: Boolean
-    ): Function<T, Observable<T>> {
-        return Function { t ->
-            if (continueOnError) {
-                storage.save(t)!!.onErrorReturn { t }
-            } else storage.save(t)
-        }
+private fun <T> deferSaveIn(
+    storage: Storage<T>,
+    continueOnError: Boolean
+): Function<T, Single<T>> {
+    return Function { t ->
+        if (continueOnError) {
+            storage.save(t).onErrorReturn { t }
+        } else storage.save(t)
     }
 }
